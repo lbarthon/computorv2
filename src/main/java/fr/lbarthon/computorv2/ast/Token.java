2582e3741955a18ee84fedf6bb3ada2f67ea67aa
lbarthon <lsbarthonet2@gmail.com>
2019-11-19 10:59:24 +0100

    Started working

--- src/main/java/fr/lbarthon/computorv2/ast/Token.java
+++ src/main/java/fr/lbarthon/computorv2/ast/Token.java
@@ -1,7 +1,5 @@
 package fr.lbarthon.computorv2.ast;
 
-import fr.lbarthon.computorv2.exceptions.NodeCalculationException;
-import fr.lbarthon.computorv2.variables.IVariable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,12 +10,12 @@ import java.util.stream.Collectors;
 @Getter
 @AllArgsConstructor
 public enum Token {
-    PLUS('+', false),
-    LESS('-', false),
-    MULT('*', true),
-    DIV('/', true),
-    MOD('%', true),
-    POW('^', true);
+    PLUS('+', true),
+    LESS('-', true),
+    POW('^', true),
+    MULT('*', false),
+    DIV('/', false),
+    MOD('%', false);
 
     char token;
     boolean prio;
@@ -28,26 +26,5 @@ public enum Token {
 
     public static List<Token> getNonPrioValues() {
         return Arrays.stream(values()).filter(t -> !t.isPrio()).collect(Collectors.toList());
-    }
-
-    public static IVariable apply(IVariable left, IVariable right, Token token) throws NodeCalculationException {
-        // TODO
-        switch (token) {
-            case PLUS:
-                break;
-            case LESS:
-                break;
-            case MULT:
-                break;
-            case DIV:
-                break;
-            case MOD:
-                break;
-            case POW:
-                break;
-            default:
-                throw new NodeCalculationException(null);
-        }
-        return left;
     }
 }
